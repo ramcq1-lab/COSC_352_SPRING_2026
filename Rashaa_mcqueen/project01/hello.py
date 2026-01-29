56bb200dfd3d0d8e925b4110a9ccbf4474155d4f
ramcq1-lab <ramcq1@morgan.edu>
2026-01-29 11:25:03 -0500

    Add command line argument handling to hello.py

--- Rashaa_mcqueen/project01/hello.py
+++ Rashaa_mcqueen/project01/hello.py
@@ -1,1 +1,10 @@
+import sys
+def main():
+   if len(sys.argv)<2:
+     print('Usage:python hello.py<name>')
+     return
+  name = sys.argv[1]
+  print(f'Hello World{name}')
 
+if __name__=="__main__":
+  main() 
